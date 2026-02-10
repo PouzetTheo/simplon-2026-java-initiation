f4b091cb16fc4b09826d001c4260f7ea1cc23286
pouzettheo <pouzettheo.canin@icloud.com>
2026-02-10 16:38:47 +0100

    exercice: 4,5 et 6

--- src/main/java/fr/simplon/exercises/Exercise04Loops.java
+++ src/main/java/fr/simplon/exercises/Exercise04Loops.java
@@ -6,54 +6,80 @@ package fr.simplon.exercises;
  * Objectif: Apprendre à utiliser les boucles for et while
  */
 public class Exercise04Loops {
-    
+
     /**
      * Calcule la somme des nombres de 1 à n
+     * 
      * @param n le nombre maximum
      * @return la somme de 1 + 2 + ... + n
      */
     public int sumUpToN(int n) {
-        throw new UnsupportedOperationException();
-
+        int somme = 0;
+        for (int i = 1; i <= n; i++) {
+            somme = somme + i;
+        }
+        return somme;
     }
-    
+
     /**
      * Inverse un nombre (ex: 123 -> 321)
+     * 
      * @param number le nombre à inverser
      * @return le nombre inversé
      */
     public int reverseNumber(int number) {
-        throw new UnsupportedOperationException();
+        int inverse = 0;
+        while (number > 0) {
+            int chiffre = number % 10; // prend le dernier chiffre
+            inverse = inverse * 10 + chiffre; // ajoute le chiffre à droite de "inverse"
+            number = number / 10; // accède au chiffre suivant
+        }
+        return inverse;
 
     }
-    
+
     /**
      * Calcule la factorielle d'un nombre (n!)
+     * 
      * @param n le nombre
      * @return n! (n factorielle)
      */
     public int factorial(int n) {
-        throw new UnsupportedOperationException();
+        if (n <= 1)
+            return 1;
+        int produit = 1;
+        for (int i = 2; i <= n; i++)
+            produit = produit * i;
+        return produit;
 
     }
-    
+
     /**
      * Compte le nombre de chiffres dans un nombre
+     * 
      * @param number le nombre
      * @return le nombre de chiffres
      */
     public int countDigits(int number) {
-        throw new UnsupportedOperationException();
-
+        String text = Integer.toString(number);
+      return text.length();
     }
-    
+
     /**
      * Vérifie si un nombre est premier
+     * 
      * @param number le nombre à vérifier
      * @return true si le nombre est premier, false sinon
      */
     public boolean isPrime(int number) {
-        throw new UnsupportedOperationException();
-
+       if (number <=1){
+        return false;
+       }
+       for (int i =2; 1<number; i++){
+           if (number% i ==0){
+            return false;
+           }
+       }
+       return true;
     }
 }
--- src/main/java/fr/simplon/exercises/Exercise06ArraysMaxMin.java
+++ src/main/java/fr/simplon/exercises/Exercise06ArraysMaxMin.java
@@ -3,57 +3,90 @@ package fr.simplon.exercises;
 /**
  * Exercice 6: Tableaux - Recherche du maximum et minimum
  * 
- * Objectif: Apprendre à trouver le plus grand et le plus petit élément d'un tableau
+ * Objectif: Apprendre à trouver le plus grand et le plus petit élément d'un
+ * tableau
  */
 public class Exercise06ArraysMaxMin {
-    
+
     /**
      * Trouve le maximum dans un tableau
+     * 
      * @param array le tableau
      * @return la valeur maximale
      */
     public int findMax(int[] array) {
-        throw new UnsupportedOperationException();
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max)
+                max = array[i];
 
+        }
+        ;
+        return max;
     }
-    
+
     /**
      * Trouve le minimum dans un tableau
+     * 
      * @param array le tableau
      * @return la valeur minimale
      */
     public int findMin(int[] array) {
-        throw new UnsupportedOperationException();
-
+        int min = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < min)
+                min = array[i];
+        }
+        return min;
     }
-    
+
     /**
      * Trouve l'index du maximum dans un tableau
+     * 
      * @param array le tableau
      * @return l'index de la valeur maximale (le premier si plusieurs)
      */
     public int findMaxIndex(int[] array) {
-        throw new UnsupportedOperationException();
-
+        int maxindex = 0;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > array[maxindex])
+                maxindex = i;
+        }
+        return maxindex;
     }
-    
+
     /**
      * Trouve l'index du minimum dans un tableau
+     * 
      * @param array le tableau
      * @return l'index de la valeur minimale (le premier si plusieurs)
      */
     public int findMinIndex(int[] array) {
-        throw new UnsupportedOperationException();
-
+        int minIndex = 0;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < array[minIndex])
+                minIndex = i;
+        }
+        return minIndex;
     }
-    
+
     /**
      * Calcule la différence entre le max et le min
+     * 
      * @param array le tableau
      * @return la différence max - min
      */
     public int range(int[] array) {
-        throw new UnsupportedOperationException();
-
+        int max =array [0];
+        int min = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max){
+                max = array[i];
+            }if (array[i] < min){
+                min = array[i];
+            }
+        }
+        return max-min;
     }
 }
+
